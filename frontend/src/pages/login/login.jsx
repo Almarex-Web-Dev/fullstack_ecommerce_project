a7787436dbab70a4d96294a819795c4444d135d2
Almarex <almarexwebdev@gmail.com>
2022-12-07 18:26:50 +0000

    added more styles to login.scss files

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -3,30 +3,32 @@ import shoppingIMG from '../../assets/shopping.png'
 
 import { FcGoogle } from 'react-icons/fc'
 
+import './login.scss'
 const Login = () => {
   return (
     <section className="login__page-wrapper">
       {/* login page content goes here */}
       <div className="logign__content-section">
-        <div className="login__content">
-          <h1>Log in, enjoy shopping!</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-            eget aenean dis enim gravida urna, aliquet fames velit. Proin morbi
-            sit bibendum tincidunt. Risus fringilla amet quis tempus condimentum
-            eu porttitor tempus. Sed sapien, bibendum id varius
-          </p>
-          <a className="round__button" href="#">
-            <div>
-              <span className="bg"></span>
-              <span className="base"></span>
-              <span className="text">Explore more</span>
-            </div>
-          </a>
+        <div className="login__content-wrapper">
+          <div className="login__content">
+            <h1>Log in, enjoy shopping!</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              eget aenean dis enim gravida urna, aliquet fames velit. Proin
+              morbi sit bibendum tincidunt. Risus fringilla amet quis tempus
+              condimentum eu porttitor tempus. Sed sapien, bibendum id varius
+            </p>
+            <a className="round__button" href="#">
+              <div>
+                <span className="bg"></span>
+                <span className="base"></span>
+                <span className="text">Explore more</span>
+              </div>
+            </a>
+          </div>
+          {/* hero image for login page */}
         </div>
-        <div className="login__image-content">
-          <img src={shoppingIMG} alt="shopping_img" />
-        </div>
+        <img src={shoppingIMG} alt="shopping_img" />
       </div>
       {/* form input goes here */}
       <div className="form__input-section">
